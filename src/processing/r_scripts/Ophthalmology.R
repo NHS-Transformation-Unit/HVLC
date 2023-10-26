9757847516ef60b7b4245dc3baf0cc9ebfc6187f
SLai-mc <80843364+SLai-mc@users.noreply.github.com>
2023-10-26 16:36:02 +0100

    Delete blank lines

--- src/processing/r_scripts/Ophthalmology.R
+++ src/processing/r_scripts/Ophthalmology.R
@@ -10,7 +10,6 @@ hvlc_ophth_lccs_filter <- function(input_df){
          !grepl("F00|F01|F02|F03|G30|F051|G310|G311|F70|F71|F72|F73|F78|F79|
                 H200|H201|H202|H208|H209|H220|H221|H300|H301|H302|H308|H309|H320", Der_Diagnosis_All)
     )
-  
 }
 
 hvlc_ophth_lccs_flag <- function(input_df){
@@ -27,9 +26,4 @@ hvlc_ophth_lccs_flag <- function(input_df){
     )
     ) %>%
     arrange(desc(Low_complexity_cataract_surgery_flag))
-  
 }
-
-
-
-
